94a15888d3dc3df20d9487ebf462c05feb40dd52
Asya Danilova <asya_danilova@epam.com>
2025-01-22 10:54:46 +0200

    fix: ReadStream method is added instead of ReadFile

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,11 +1,8 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-fs.readFile(
-  path.join(__dirname, "text.txt"),
-  "utf-8",
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  },
-);
+const filePath = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(filePath, 'utf-8');
+stream.on('data', (text) => {
+  console.log(text);
+});
